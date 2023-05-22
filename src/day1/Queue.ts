2094acacdea72a660b571b3b9ff7e05c81b89b04
Mateus Pandino <matpandino@gmail.com>
2023-05-22 12:26:18 -0300

    add queue implementation + typo

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,47 @@
+type Node<T> = {
+    value: T;
+    next?: Node<T>;
+};
+
 export default class Queue<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
+    private tail?: Node<T>;
 
     constructor() {
+        this.head = this.tail = undefined;
+        this.length = 0;
     }
 
     enqueue(item: T): void {
+        const node = { value: item } as Node<T>;
+        this.length++;
+        if (!this.tail) {
+            this.tail = this.head = node;
+            return;
+        }
 
-}
-    deque(): T | undefined {
+        this.tail.next = node;
+        this.tail = node;
+    }
 
-}
+    dequeue(): T | undefined {
+        if (!this.head) return undefined;
+
+        this.length--;
+
+        const head = this.head;
+        this.head = this.head.next;
+
+        if(this.length === 0) this.tail = undefined
+        
+        // free (not needed, js has garbage collector)
+        head.next = undefined;
+
+        return head.value;
+    }
+
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
